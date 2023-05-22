d3270459b81de6134855485b09ac965df66561bf
SziNo <szino1848@gmail.com>
2023-05-22 14:04:24 +0200

    move most of the content to Quizesview

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,11 @@
 <script setup>
-import q from './data/quizes.json'
-import { ref, watch } from 'vue'
-import Card from './components/Card.vue'
-
-const quizes = ref(q)
-const search = ref('')
-
-watch(search, () => {
-  quizes.value = q.filter((el) =>
-    el.name.toLowerCase().includes(search.value.toLowerCase())
-  )
-})
+import { RouterView } from 'vue-router'
+import QuizesView from './views/QuizesView.vue'
 </script>
 
 <template>
   <div class="container">
-    <header>
-      <label for="quiz">Quizes</label>
-      <input
-        v-model.trim="search"
-        id="quiz"
-        type="text"
-        placeholder="Search..."
-      />
-    </header>
-
-    <div class="options-container">
-      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
-    </div>
+    <RouterView />
   </div>
 </template>
 
@@ -35,31 +13,5 @@ watch(search, () => {
 .container {
   max-width: 1000px;
   margin: 0 auto;
-}
-
-header {
-  margin-bottom: 10px;
-  margin-top: 30px;
-  display: flex;
-  align-items: center;
-}
-
-header label {
-  font-weight: bold;
-  margin-right: 30px;
-  font-size: 30px;
-}
-
-header input {
-  border: none;
-  background-color: rgba(128, 128, 128, 0.1);
-  padding: 10px;
-  border-radius: 5px;
-}
-
-.options-container {
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: 40px;
 }
 </style>
